0e291e1c3d11b14310ab49ee1d45701898ed2004
Ankit Dhungana <80185690+ankit-dhungana@users.noreply.github.com>
2023-05-30 19:12:40 -0500

    Painting features added by updating JS and CSS.

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ class PixelPainter {
     this.colorPicker = document.querySelector("#paint-color");
     this.resizeButton = document.querySelector("#resize");
     this.clearButton = document.querySelector("#clear");
-
     this.rainbowButton = document.querySelector("#rainbow");
+
+    this.setupEventListeners();
   }
 
   // methods
@@ -25,7 +26,47 @@ class PixelPainter {
       }
     }
   }
+
+  resizeGrid() {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+      cell.remove();
+    });
+    const cellCount = prompt("Enter the number of cells per side (Max 80):");
+
+    if (cellCount > 80) {
+      this.createGrid(80);
+    } else if (cellCount < 1) {
+      this.createGrid(2);
+    } else {
+      this.createGrid(cellCount);
+    }
+    this.drawPixel(this.currentColor);
+  }
+  drawPixel(color = "black") {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+      cell.addEventListener("mouseover", (e) => {
+        if (e.buttons === 1) {
+          return;
+        }
+        cell.style.backgroundColor = color;
+      });
+
+      cell.addEventListener("click", () => {
+        cell.style.backgroundColor = color;
+      });
+    });
+  }
+  setupEventListeners() {
+    this.resizeButton.addEventListener("click", () => {
+      this.resizeGrid();
+      this.rainbowButton.classList.remove("rainbow-btn");
+      this.rainbowButton.classList.add("control-button");
+    });
+  }
 }
 
 const painter = new PixelPainter();
 painter.createGrid(); //overload this to change the grid size
+painter.drawPixel();
